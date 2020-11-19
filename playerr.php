c0ef8ad3d8e5cbc364ec91bc6bc661171016f294
aamir-hassan <74731708+aamir-hassan@users.noreply.github.com>
2020-11-20 01:40:24 +0500

    Add files via upload

---
+++ playerr.php
@@ -0,0 +1,388 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Baahubali 2 - The Conclusion - Official Trailer (Hindi) - S.S. Rajamouli - Prabhas - Rana Daggubati</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92260810-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- PopAds.net Popunder Code for watchify.net -->
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+  var _pop = _pop || [];
+  _pop.push(['siteId', 1858715]);
+  _pop.push(['minBid', 0]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 300]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', true]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0]; 
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
+/*]]>/* */
+</script>
+<!-- PopAds.net Popunder Code End -->
+
+<script type="text/javascript" src="http://watchify.net/styles/cb_28/theme/js/jquery-1.11.3.min.js"></script>
+<script>
+var baseurl = 'http://watchify.net';
+var imageurl = 'http://watchify.net/styles/cb_28/images';
+</script>
+
+<script type="text/javascript">
+var embedPlayerWidth 	=	'300';
+var embedPlayerHeight 	=	'250';
+var autoPlayEmbed 		=	'no';
+var embed_type 			=	'iframe';
+
+
+
+function updateEmbed(width,height,autoplay)
+{
+	$('#embed_code').val( $('#embed_code').val().replace(/width=\"([0-9]+)\"/g,'width="'+width+'"') );
+	$('#embed_code').val( $('#embed_code').val().replace(/height=\"([0-9]+)\"/g,'height="'+height+'"') );
+	$('#embed_code').val( $('#embed_code').val().replace(/width=([0-9]+)/g,'width='+width) );
+	$('#embed_code').val( $('#embed_code').val().replace(/height=([0-9]+)/g,'height='+height+'') );
+}
+
+function switchEmbedCode(type)
+{
+	if(embed_type==type)
+		return false;
+	else
+	{
+		embed_type = type;
+		
+		var alt_embed =$('#alternate_embed_code').val();
+		$('#alternate_embed_code').val($('#embed_code').val());
+		$('#embed_code').val(alt_embed);
+		
+	}
+}
+
+
+
+</script>
+
+
+<!-- Including JS Files-->
+	<script type="text/javascript" src="http://watchify.net/js/jquery_plugs/cookie.min.js"></script>
+	<script type="text/javascript" src="http://watchify.net/js/functions.min.js"></script>
+<!-- Including JS Files-->
+<script type="text/javascript">
+	callURLParser();
+</script>
+<!-- Including Plugin Headers -->
+	<script type="text/javascript">
+var uads_ajax = 'http://watchify.net/plugins/player_ads_manager/ads_ajax.php';
+
+function update_ad_imp(ad_id){
+	var ad_id = ad_id;
+	$.ajax({
+		url : uads_ajax,
+		data : { ad_id : ad_id, mode : "update_imp"},
+		type : "post",
+		datatype : "json",
+		error : function(data){
+			console.log(data);
+		},
+		success : function(data){
+			if (data.msg){
+				msg = data.msg;
+			}else{
+				msg = data.err;
+			}
+			console.log(msg);
+		}
+
+	});
+}
+	
+</script>
+<!-- Ending Plugin Headers -->
+
+
+
+<script type="text/javascript">
+
+window.onload = function() {
+		ini_cookies();
+}
+
+</script>
+
+
+	<meta property="fb:app_id" content="541598765890838" />
+        <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="http://watchify.net/files/thumbs/processing.jpg" />
+    <meta property="og:image:width" content="768" />
+	<meta property="og:image:height" content="432" />
+	<meta property="og:image:type" content="image/jpeg" />
+            <meta property="og:type" content="video">
+        <meta property="og:video" content='http://watchify.net/player/pak_player/pakplayer.swf?config=http%3A%2F%2Fwatchify.net%2Fplayer%2Fpak_player%2Fembed_player.php%3Fvid%3D%26json%3Dtrue%26autoplay%3Dyes'>
+        <meta property="og:video:height" content="259" />
+        <meta property="og:video:width" content="398" />
+        <meta property="og:video:type" content="application/x-shockwave-flash">
+            <meta property='og:url' content='http://watchify.net'/>
+        <meta property='og:site_name' content=''/>
+    
+
+
+
+
+
+
+<script>
+$(function() {
+
+	/*$( ".date_field" ).datepicker({ 
+		dateFormat: 'yy-mm-dd',
+		changeMonth: true,
+		changeYear: true,
+		yearRange : "1901 : c"
+		});*/
+});
+
+</script>
+
+
+
+
+
+<!-- ClipBucket version 2.8.1 --></head>
+
+<body style="margin:0px; padding:0px">
+<!-- Following is the code for cb ultimate invideo ads settings  -->
+<!-- End  -->
+
+<!-- This code is added for timecomments plugin-->
+<!-- End -->
+
+<!-- This code is added for instance manager Interactive ads plugin-->
+<!-- End -->
+
+<!-- This code is added for instance manager Interactive ads plugin intances-->
+			<!-- End -->
+
+<!-- This code is added for Related Videos carousel plugin-->
+<!-- End -->
+
+<link rel="stylesheet" href="http://watchify.net/player/CB_video_js/css/video-js.min.css">
+<link href="http://watchify.net/player/CB_video_js/css/cb_vjs_plugin.css" rel="stylesheet">
+<link href="http://watchify.net/player/CB_video_js/css/videojs.ads.css" rel="stylesheet">
+<link href="http://watchify.net/player/CB_video_js/css/videojs.ima.css" rel="stylesheet">
+<link href="http://watchify.net/player/CB_video_js/css/cb_vjs_resolution.css" rel="stylesheet">
+
+<script src="http://watchify.net/player/CB_video_js/js/video-js.min.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/cb_vjs_custom.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/cb_vjs_plugin.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/cb_vjs_resolution.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/videojs.ads.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/videojs.ima.js"></script>
+<script src="http://watchify.net/player/CB_video_js/js/cb_ultimate_ads.js"></script>
+<script type="text/javascript" src="http://watchify.net/player/CB_video_js/js/videojs.relatedCarousel.js"></script>
+
+<video id="cb_video_js" class="video-js vjs-default-skin" height="250px" width="300px" poster="http://watchify.net/files/thumbs/2017/03/16/14896628449645c-768x432-5.jpg" >
+        
+        	<source src='http://watchify.net/files/videos/2017/03/16/14896628449645c-1080.mp4' type="video/mp4" label="1080" res="1080"  />
+            
+        	<source src='http://watchify.net/files/videos/2017/03/16/14896628449645c-240.mp4' type="video/mp4" label="240" res="240"  />
+            
+        	<source src='http://watchify.net/files/videos/2017/03/16/14896628449645c-360.mp4' type="video/mp4" label="360" res="360"  />
+            
+        	<source src='http://watchify.net/files/videos/2017/03/16/14896628449645c-480.mp4' type="video/mp4" label="480" res="480"  />
+            
+        	<source src='http://watchify.net/files/videos/2017/03/16/14896628449645c-720.mp4' type="video/mp4" label="720" res="720"  />
+            
+    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+</video>
+
+
+<script type="text/javascript">
+	var vid_id = "3065";
+	var videotitle = "Baahubali 2 - The Conclusion - Official Trailer (Hindi) - S.S. Rajamouli - Prabhas - Rana Daggubati";
+	var uploader = "badtameezdil.net";
+	var branding_logo  = "iVBORw0KGgoAAAANSUhEUgAAACMAAAAZCAYAAAC7OJeSAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAaVJREFUSEvtlmFtwzAQhQugBEqgBEKgCIagDMZgFEKhGEqiLAom3fecN2tOz3ZSaT8m9ZOsxHcvd2fHsbN78694PB7DNE0X2o37J7BfaZ/Scj3QRtnsLsB+l4/2kYID5lOkxzZy2Vs2M7v68PAxClpD+hXxh1TEDxhU+X32xdi/p3W1As2Xw2s2zzYXYB8tKcFxtCbkd3C6KqqJpRlMw+yZId7VrhgE1RHLZ1lCxdkVgj+vGUF/OTsnu2J6CSAHUDLbQvAXI6efPw7ui4GFIDpYH4L/YmkRvEFanMu49PMrb4Kw+bUo8DJ4DcVyzGLG6aevrAvC3vRrj7m42yUo5OZU6/BzfwLFnJ1mHcvRtNgyS6bccXuQoLnnLNivLV6FO8U2eLD7taDJ55VNPdp7Sw0SNBeyUBGWS998Xfj7e0sNnm9u+UpuaQLTafbEoE+z+DIE0BFf42nKNXr7nsCXZ/ElCBAuZCW1pAD7ttN5K0rsmBls4ZTjqr3a8p/lVUgcLeTqXqFZsCZBv/2rsBUC5n1ExdkcgkT7TjrfuOoXdN059CZkt/sGoLVWBjDEUFYAAAAASUVORK5CYII=";
+	var product_link = "http://watchify.net/";
+	var show_logo = true;
+	var autoplay = "no";
+	var default_res = "360";
+	
+	if (autoplay == 'no'){
+		autoplay = false;
+	}else{
+		autoplay = true;
+	}
+	//player settings
+	var cb_player_setup = {
+		plugins : { }, 
+		"techOrder" : ['html5', 'flash'],
+		"controls": true,
+		"autoplay": autoplay,
+		"preload": "auto",
+	};
+
+	var cb_timecomments_plugin = ""; // timecomments settings
+	if (cb_timecomments_plugin){
+		var user_id = "";
+		var username = "";
+		var user_profile  = "http://watchify.net/images/avatars/no_avatar.png";
+	}
+
+	var play_ad = ""; //  Ad settings starts to pass on to player
+	if (play_ad){
+		var ad_code = "";
+		var ad_id = "";
+	}
+
+	var related_videos = 'null'; // Related videos plugin settings
+	var slot_id = "";
+	related_videos_active= true;
+
+	var video_editor_enabled = ""; // Interactive Ads plugin settings
+	var i_instances = '';
+
+	//Setting CallBack
+	var cb_vjs_callback = function(){
+		var cb_vjs = this;
+		//Calling Custom videoJs Elements Plugin
+		var el_options = {
+			logo : { 
+					branding_logo : branding_logo,
+				    product_link : product_link,
+				    show_logo : true 
+			},
+			header : { 
+					uploader : uploader,
+					videotitle : videotitle,
+					videoid : vid_id
+			}
+		}
+		cb_vjs.cb_vjs_elements(el_options);
+
+		//Calling Resolution switcher plugin
+		var res_options = {
+			default : default_res,
+			dynamicLabel : false
+		}
+		cb_vjs.videoJsResolutionSwitcher(res_options);
+
+		//Calling Resolution switcher plugin
+		if (play_ad){
+			var ad_options = {
+			  	ad_id: ad_id,
+			    ad_code: ad_code,
+			    autoplay: autoplay
+			};
+			cb_vjs.cb_ultimate_ads(ad_options);
+		}
+
+		//Calling TimeComments switcher plugin
+		if ( cb_timecomments_plugin == 'installed'  && typeof cb_timecomments_plugin != 'undefined'){
+			
+			var timecomments = '';
+			var comments_options = {
+				comments : timecomments,
+				dummy : false,
+				videoid : vid_id,
+				userid : user_id,
+				userprofile : user_profile,
+				username : username,
+				forceShow : true
+			}
+			cb_vjs.timecomments(comments_options);
+			if (typeof timecomments != 'undefined' && timecomments != ''){
+				console.log("Initialized Time Comments");
+			}else{
+				console.log("Time Comments are empty.. /");
+			}
+			
+		}
+
+		//Calling related Videos Plugin  
+		if ( related_videos != "" && typeof related_videos != 'undefined' && related_videos_active &&   
+			!video_editor_enabled ){
+			related_videos = JSON.parse(related_videos);
+			cb_vjs.relatedCarousel(related_videos);
+		}
+
+		//Calling instance manager plugin for backend
+		if ( video_editor_enabled && typeof video_editor_enabled != 'undefined'){
+			instance_manager_settings = {
+				slot_id : slot_id
+			}
+			cb_vjs.instanceManager(instance_manager_settings);
+		}
+
+		//Calling interactive ads plugin
+		if (i_instances){
+			i_instances = JSON.parse(i_instances);
+			i_instances_settings = {
+				enable : true,
+				dummy : false,
+				instances : i_instances,
+			}
+			cb_vjs.interactiveAds(i_instances_settings);	
+		}
+
+		var  stateCheck = setInterval(function(){
+		  	if (document.readyState === 'complete') {
+		    	clearInterval(stateCheck);
+		  		var cbVjsMenuBtn = document.querySelector('.vjs-resolution-button');
+		  		if (cbVjsMenuBtn){
+		  			var cbVjsMenu = cbVjsMenuBtn.children[0];
+					var cbVjsMenuContent = cbVjsMenu.children[0];
+					var cbVjsMenuContentRes = cbVjsMenuContent.children;
+					
+					for (var i = 0; i < cbVjsMenuContentRes.length; i++ ){
+						cbVjsMenuContent.insertBefore(cbVjsMenuContentRes[i],cbVjsMenuContent.firstChild);
+					}
+		  		}
+		  	}
+		},100);  
+
+
+	}
+	//Initializing
+	var cb_player = videojs('cb_video_js', cb_player_setup , cb_vjs_callback);
+    
+
+    
+
+</script>
+
+
+<style type="text/css">
+	.video-js {
+		width: 100%;
+	}
+	.video-js .vjs-tech {
+		height: 100% !important;
+	}
+	.editorpick .cb_video_js-dimensions{
+		width: 100% !important;
+		height: 100% !important;
+	}
+</style>
+
+</body>
+</html>
